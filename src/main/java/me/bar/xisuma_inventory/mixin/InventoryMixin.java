765a3678cb22333b3f385d8a2a5908e19314201b
bar2011 <bareinav10@gmail.com>
2022-08-18 00:30:55 +0300

    Created the inventory mixin (a way to change the player inventory)

---
+++ src/main/java/me/bar/xisuma_inventory/mixin/InventoryMixin.java
@@ -0,0 +1,8 @@
+package me.bar.xisuma_inventory.mixin;
+
+import net.minecraft.entity.player.PlayerInventory;
+
+@org.spongepowered.asm.mixin.Mixin(PlayerInventory.class)
+public class InventoryMixin {
+
+}
